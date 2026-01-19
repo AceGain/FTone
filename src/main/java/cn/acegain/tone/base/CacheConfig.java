c75dcd28be36bb2b0d3a11160103a20fd3855111
acegain <acegain@163.com>
2026-01-19 16:25:03 +0800

    新增内存缓存配置。

--- src/main/java/cn/acegain/tone/base/CacheConfig.java
+++ src/main/java/cn/acegain/tone/base/CacheConfig.java
@@ -2,10 +2,12 @@ package cn.acegain.tone.base;
 
 import cn.hutool.cache.impl.TimedCache;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.event.ContextClosedEvent;
 import org.springframework.context.event.ContextRefreshedEvent;
 
@@ -17,6 +19,15 @@ import java.time.Duration;
 public class CacheConfig {
 
     @Bean
+    @Primary
+    public TimedCache<String, Object> timedCache() {
+        TimedCache<String, Object> timedCache = new TimedCache<>(Duration.ofMinutes(5).toMillis());
+        timedCache.schedulePrune(10 * 1000);
+        return timedCache;
+    }
+
+    @Bean
+    @Qualifier("tokenCache")
     public TimedCache<String, Object> tokenCache() {
         TimedCache<String, Object> timedCache = new TimedCache<>(Duration.ofMinutes(5).toMillis());
         timedCache.schedulePrune(10 * 1000);
