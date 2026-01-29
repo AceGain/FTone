7b4c8e71a2d91a8e8debe35091d8188dc4fd9e5a
acegain <acegain@163.com>
2026-01-29 16:24:30 +0800

    暂存.

--- src/main/java/cn/acegain/tone/base/security/WebSecurityConfig.java
+++ src/main/java/cn/acegain/tone/base/security/WebSecurityConfig.java
@@ -1,20 +1,28 @@
 package cn.acegain.tone.base.security;
 
+import cn.acegain.tone.base.jwt.JwtService;
+import cn.acegain.tone.system.service.SysUserService;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity
@@ -22,15 +30,45 @@ import org.springframework.security.web.SecurityFilterChain;
 @RequiredArgsConstructor
 public class WebSecurityConfig {
 
-    //    private final UserDetailsServiceImpl userDetailsService;
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+//    @Bean
+//    public UserDetailsService userDetailsService() {
+//        return new AuthService();
+//    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider(AuthService authService) {
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider(authService);
+        daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
+        return daoAuthenticationProvider;
+    }
+
+    @Bean
+    public JwtAuthenticationProvider jwtAuthenticationProvider(JwtService jwtService, AuthService authService) {
+        return new JwtAuthenticationProvider(jwtService, authService);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(JwtAuthenticationProvider jwtAuthenticationProvider,
+                                                       DaoAuthenticationProvider daoAuthenticationProvider) {
+        return new ProviderManager(List.of(jwtAuthenticationProvider, daoAuthenticationProvider));
+    }
+
+    @Bean
+    public JwtAuthenticationFilter jwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        return new JwtAuthenticationFilter(authenticationManager);
+    }
+
+    @Bean
+    @SneakyThrows
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthenticationFilter jwtAuthenticationFilter) {
         httpSecurity
-                // 启用跨域
                 .cors(Customizer.withDefaults())
-                // 不使用Session，禁用csrf
                 .csrf(CsrfConfigurer::disable)
-                // 使用Token，不创建Session
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(requests -> requests
@@ -38,20 +76,9 @@ public class WebSecurityConfig {
                         .requestMatchers(HttpMethod.GET, "/info").permitAll()
                         .requestMatchers(HttpMethod.GET, "/captcha").permitAll()
                         .requestMatchers(HttpMethod.POST, "/auth").permitAll()
-                        .anyRequest().authenticated());
-        //.authenticationProvider(daoAuthenticationProvider);
-        // .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                        .anyRequest().authenticated())
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
-    }
-
-    @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
-        return authenticationConfiguration.getAuthenticationManager();
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
 }
