15e685583a703a2b1ae3f219cff3a50c3fcb205c
AceGain <acegain@163.com>
2026-02-14 03:05:15 +0800

    配置全局异常拦截器。

--- src/main/java/cn/acegain/tone/base/GlobalExceptionHandler.java
+++ src/main/java/cn/acegain/tone/base/GlobalExceptionHandler.java
@@ -3,6 +3,7 @@ package cn.acegain.tone.base;
 import cn.acegain.tone.common.Result;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -11,6 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @Order
+    @ExceptionHandler(Exception.class)
+    public Result<String> exceptionHandler(Exception e) {
+        log.error(e.getMessage());
+        return Result.error(HttpServletResponse.SC_INTERNAL_SERVER_ERROR, "服务异常！");
+    }
+
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public Result<String> httpMessageNotReadableExceptionHandler(HttpMessageNotReadableException e) {
         Object code = HttpServletResponse.SC_BAD_REQUEST;
@@ -18,7 +26,8 @@ public class GlobalExceptionHandler {
         if (message.contains("Required request body is missing")) {
             return Result.error(code, "请求体不能为空！");
         }
-        return Result.error(code, message);
+        log.error(message);
+        return Result.error(code, "服务异常！");
     }
 
 }
